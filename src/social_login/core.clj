bf1a6d91a88ef7dd6c6f759b4c9f6413f7d77e1e
Author Name <gabriel@trajber.com.br>
2021-05-15 16:13:13 -0300

    initial commit

---
+++ src/social_login/core.clj
@@ -0,0 +1,6 @@
+(ns social-login.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/social_login/core_test.clj
@@ -0,0 +1,7 @@
+(ns social-login.core-test
+  (:require [clojure.test :refer :all]
+            [social-login.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
